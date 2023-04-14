61d7914c0b8c6caa374eed61fc0e7e92b71a5a99
moofat7y <mohammedbelal802@gmail.com>
2023-04-14 22:10:21 +0200

    third commit

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -36,8 +36,11 @@ var userSchema = new mongoose.Schema(
     },
     image: {
       type: Object,
-      secure_url:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/120px-User-avatar.svg.png?20201213175635",
+      secure_url: {
+        type: String,
+        default:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/120px-User-avatar.svg.png?20201213175635",
+      },
       public_id: null,
     },
     isBlocked: {
